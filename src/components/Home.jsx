281928b744a2ec45812a98f35a229e70efdc7c5f
Mustafa Hijazi <hijazimt01@gmail.com>
2021-12-22 23:53:34 +0200

    edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Intro from "./intro";
-import Navigation from "./Navigation"
+//import Navigation from "./Navigation"
 
 function Home() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./components/Home";
+//import Home from "./components/Home";
 import "./css/index.css";
 //import * as serviceWorker from "./serviceWorker";
 import Webpages from "./components/Webpages";
