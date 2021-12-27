a893eb7d383b5aa5bc5bcc0e9069f572ccf9142c
Mustafa Hijazi <hijazimt01@gmail.com>
2021-12-27 18:36:32 +0200

    Update mods.jsx

--- src/components/mods.jsx
+++ src/components/mods.jsx
@@ -23,7 +23,7 @@ function Modsinfo(props)
             <h5>{props.country}</h5>
             <h5>{props.gender}{props.gender2}</h5>
             <h5 className="modbio">{props.bio}</h5>
-            <div className="email">{props.email === "N/A" ? <span style={{color: `white`}} href="" className="modemail">{props.email}</span> : <a href={emailto()} className="modemail">{props.email}</a>}</div>
+            <div className="email">{props.email === "N/A" ? <span style={{color: `white`}} className="modemail">{props.email}</span> : <a href={emailto()} className="modemail">{props.email}</a>}</div>
             <div className="modlinkcontain">
 
             {props.instagram ? <a href={props.instagram} className="modlinks"><img src={insta} alt="" className="modlinkimg"/></a> : ""}
