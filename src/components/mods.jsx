0298b61f1ec70d95a9ab4db3bc765f97fa032632
Mustafa Hijazi <hijazimt01@gmail.com>
2021-12-27 18:35:53 +0200

    Update mods.jsx

--- src/components/mods.jsx
+++ src/components/mods.jsx
@@ -23,12 +23,10 @@ function Modsinfo(props)
             <h5>{props.country}</h5>
             <h5>{props.gender}{props.gender2}</h5>
             <h5 className="modbio">{props.bio}</h5>
-            <div className="email">{props.email === "N/A" ? <a style={{color: `white`}} className="modemail">{props.email}</a> : <a href={emailto()} className="modemail">{props.email}</a>}</div>
+            <div className="email">{props.email === "N/A" ? <span style={{color: `white`}} href="" className="modemail">{props.email}</span> : <a href={emailto()} className="modemail">{props.email}</a>}</div>
             <div className="modlinkcontain">
 
-            {props.instagram ? <a href={props.instagram} className="modlinks">
-                <img src={insta} alt="" className="modlinkimg"/>
-                </a> : ""}
+            {props.instagram ? <a href={props.instagram} className="modlinks"><img src={insta} alt="" className="modlinkimg"/></a> : ""}
             {props.twitter ? <a href={props.twitter} className="modlinks"><img src={twit} alt="" className="modlinkimg"/></a> : ""}
             {props.tumblr ? <a href={props.tumblr} className="modlinks"><img src={tumb} alt="" className="modlinkimg"/></a> : ""}
             {props.youtube ? <a href={props.youtube} className="modlinks"><img src={yout} alt="" className="modlinkimg"/></a> : ""}
