f47f8ef0be543e2224f70150b8bc9fdef3ff749b
Mustafa Hijazi <hijazimt01@gmail.com>
2021-12-28 21:55:43 +0200

    Manifesto done

--- src/components/manicontent.jsx
+++ src/components/manicontent.jsx
@@ -7,58 +7,65 @@ function ManifestoCont() {
             <h2 className="content-subtitle">The Third Eye Collective Manifesto</h2>
             <h3 className="content-subtitle2">Written by @HiHereAmi</h3>
 
-            <h2 className="content-subtitle">WHAT’S A COLLECTIVE?</h2>
-            <p>A collective is a group of people with a common goal. They usually see a problem and want to change it. In their exchange, they decide different measures to achieve that goal.</p>
+            <h2 className="content-subtitle red space">WHAT’S A COLLECTIVE?</h2>
+            <p>A collective is a group of people with a <b className="red">common goal.</b> They usually <b className="red">see a problem and want to change it.</b> In their exchange, they decide different measures to achieve that goal.</p>
             
-            <h2 className="content-subtitle">INITIATIVE/WHY I’M STARTING IT</h2>
-            <p>How much do you charge for a full colored full body commission? How many times did you feel you lost on opportunities because you cannot be in important conventions? How many job opportunities did you have to pass because they were looking for someone in the US or UK, even though it was a remote job? How much do you feel your university credits aren’t worth it, just because the industry doesn’t recognize the college’s name? Do you know how to get into the industry you want?</p>
-            <p>Third world artists, as a consequence of global politics, usually are in disadvantage compared to our first world artist counterparts. Even though we move in the international artists circles and markets, we always charge less for our work.</p>
+            <h2 className="content-subtitle orange space">INITIATIVE/WHY I’M STARTING IT</h2>
+            <p>How much do you charge for a full colored full body commission? How many times did you feel you <b className="orange">lost on opportunities</b> because you cannot be in important conventions? How many job opportunities did you have to pass because they were looking for someone in the US or UK, even though it was a remote job? How much do you feel your university credits aren’t worth it, just because the industry doesn’t recognize the college’s name? Do you know how to get into the industry you want?</p>
+            <p><b className="orange">Third world artists, as a consequence of global politics, usually are in disadvantage compared to our first world artist counterparts.</b> Even though we move in the international artists circles and markets, <b className="orange">we always charge less for our work.</b></p>
             <p>This happens with both individual clients and studios. We charge less money in commissions because we convert from our currency - which always ends in us losing. We shouldn’t charge five, twenty, forty dollars for full paintings if the market value is way higher. I want other third world artists to know the market value is way higher.</p>
             <p>Big studios, on their part, usually contract third world artists and studios because they can get away with paying them way less than first world artists. This isn’t fair either because the work goes to the same market.</p>        
-            <p>Third world artists being underpaid is not an issue that affects only ourselves — we are lowering the base price for everyone else. First world artists are competing with people being paid pennies and that makes them charge less which in turn makes us charge less. We need to raise this tide, all of the artists, and demand to be paid what we are worth, all of us. If all of us raise our prices, studios and clients have no excuse to underpay us.</p>
-            <p>This can only happen if we organize and inform ourselves. This collective is an attempt to remedy that. At first, we are grouping on a [Discord server](https://discord.gg/Y5ZVXDyp9x) to interact and exchange information. The idea is to get enough frequently asked questions answered so we can make a database. Make information accessible to new third world artists in the industry.</p>
+            <p><b className="orange">Third world artists being underpaid is not an issue that affects only ourselves — we are lowering the base price for everyone else.</b> First world artists are competing with people being paid pennies and that makes them charge less which in turn makes us charge less. We need to raise this tide, all of the artists, and <b className="orange">demand to be paid what we are worth, all of us.</b> If all of us raise our prices, studios and clients have no excuse to underpay us.</p>
+            <p>This can only happen if we organize and inform ourselves. This collective is an attempt to remedy that. At first, we are grouping on a <a href="https://discord.gg/Y5ZVXDyp9x" className="orange">Discord server</a> to <b className="orange">interact and exchange information.</b> The idea is to get enough frequently asked questions answered so we can make a database. Make information accessible to new third world artists in the industry.</p>
             <p>If we reach enough people I'd like to regionalize information too. I know the issues we have in Argentina are not as Philippines's. I even encourage artists of similar regions to organize themselves in the same manner - in collectives to spread information and, hopefully, in unions to fight for their rights.</p>
 
-            <h2 className="content-subtitle">SHORT TERM GOALS</h2>
+            <h2 className="content-subtitle space"><mark className="yellow">SHORT TERM GOALS</mark></h2>
             <ul>
-                <li>Connect third world artists with each other and with first world artists.</li>
-                <li>Establish a public database with information that can help newcomers to the industry that are third world artists.</li>
-                <li>Encourage the establishment of regional collectives that serve the same informative & active purpose. For example, if many SEA artists meet on the Third Eye Collective, they are encouraged to organize and collect information that pertains to their regional situation. We will publish it on our database if they wish so and/or will redirect people to their own collective if they have one. The idea is, even if regional collectives arise, to keep the connection between third world artists from totally different parts of the world.</li>
+                <li>Make a list of <mark className="yellow">general questions third world artists have and their answers</mark> - adapted to our specific regional situations. Ex.: How to take payments, how do agents work, how to move to work in a first world country is like, etc.</li>
+                <li><mark className="yellow">Collect resources and information about the industry</mark> that we usually cannot get because we are third world.</li>
+                <li>Make <mark className="yellow">optional challenges and events</mark> so members of the collective can work on our portfolios. Encourage an improvement of our work.</li>
             </ul>
 
-            <h2 className="content-subtitle">LONG TERM GOALS</h2>
+            <h2 className="content-subtitle space"><mark className="yellow">MEDIUM TERM GOALS</mark></h2>
             <ul>
-                <li>Elevate the base rates internationally in fields like illustration, animation and graphic design.</li>
-                <li>Establish an international and organized net of third world artists from different regions of the world.</li>
-                <li>Give the opportunity to professionals around the world to connect and make projects and products together.</li>
+                <li><mark className="yellow">Connect third world artists</mark> with each other and with first world artists.</li>
+                <li>Establish a <mark className="yellow">public database with information</mark> that can help newcomers to the industry that are third world artists.</li>
+                <li>Encourage the <mark className="yellow">establishment of regional collectives</mark> that serve the same informative & active purpose. For example, if many SEA artists meet on the Third Eye Collective, they are encouraged to organize and collect information that pertains to their regional situation. We will publish it on our database if they wish so and/or will redirect people to their own collective if they have one. The idea is, even if regional collectives arise, to <mark className="yellow">keep the connection between third world artists from totally different parts of the world.</mark></li>
             </ul>
 
-            <h2 className="content-subtitle">DREAM GOALS</h2>
+            <h2 className="content-subtitle space"><mark className="yellow">LONG TERM GOALS</mark></h2>
             <ul>
-                <li>Decentralize the animation, comic & illustration industry from the America-Canada-Europe sphere.</li>
-                <li>Encourage the local animation industry in third world countries.</li>
-                <li>Encourage collective works between industries in third world countries.</li>
+                <li><mark className="yellow">Elevate the base rates internationally</mark> in fields like illustration, animation and graphic design.</li>
+                <li>Establish an <mark className="yellow">international and organized net of third world artists</mark> from different regions of the world.</li>
+                <li><mark className="yellow">Give the opportunity to professionals around the world to connect and make projects and products together.</mark></li>
             </ul>
 
-            <h2 className="content-subtitle">STRUCTURE OF THE COLLECTIVE</h2>
+            <h2 className="content-subtitle space"><mark className="yellow">DREAM GOALS</mark></h2>
+            <ul>
+                <li><mark className="yellow">Decentralize</mark> the animation, comic & illustration industry from the America-Canada-Europe sphere.</li>
+                <li><mark className="yellow">Encourage the local animation industry</mark> in third world countries.</li>
+                <li><mark className="yellow">Encourage collective works between industries in third world countries.</mark></li>
+            </ul>
 
-            <h2 className="content-subtitle">THE DISCORD SERVER</h2>
-            <p>I want this to be clear from the get-go: I do not hold a special kind of authority for creating and modding the collective. [The mods](https://www.notion.so/82558b6692b34a7f90412afe627d5df8) have the responsibility to organize information, organize the discord server roles and moderate that interaction between collective participants stays respectful.</p>
-            <p>The goals, activities and/or measures taken by the collective are always up for debate. We are always open on suggestions on how to help. This collective is meant to be a tool for the artist, not the other way around.</p>
-            <p>The server will be open for three main activities: giving guidance, receiving guidance and fun, informal art exchange. The idea is that the members have both the space to have fun, collect and organize information and receive specific guidance. We wish we get to collect enough information than the need for specific guidance lessens and lessens.</p>
+            <h2 className="content-subtitle green space">STRUCTURE OF THE COLLECTIVE</h2>
+
+            <h2 className="content-subtitle green space">THE DISCORD SERVER</h2>
+            <p>I want this to be clear from the get-go: I do not hold a special kind of authority for creating and modding the collective. <a href="/thirdeye" className="green">The mods</a> have the responsibility to organize information, organize the discord server roles and moderate that interaction between collective participants stays respectful.</p>
+            <p><b className="green">The goals, activities and/or measures taken by the collective are always up for debate.</b> We are always open on suggestions on how to help. This collective is meant to be a tool for the artist, not the other way around.</p>
+            <p>The server will be open for three main activities: <b className="green">giving guidance, receiving guidance, fun, and informal art exchange.</b> The idea is that the members have both the space to have fun, collect and organize information and receive specific guidance. We wish we get to collect enough information than the need for specific guidance lessens and lessens.</p>
             
-            <h2 className="content-subtitle">UNDERSTANDING OF THE COLLECTIVE</h2>
+            <h2 className="content-subtitle blue space">UNDERSTANDING OF THE COLLECTIVE</h2>
 
-            <h2 className="content-subtitle">CONNECT, NOT DIVIDE</h2>
-            <p>The term "third world" is known by all it applies to. We are usually also called "developing countries". We are under a specific global alignment that sees us as cheap labor that provides the first world of goods.</p>
-            <p>This collective is for third world artists and freelancers who are already operating in the first world industry or intend to. We are usually paid in dollars or euros, even though that’s not our local currency.</p>
-            <p>Canada, Western Europe, Australia and Japan. And there are specific marginalizations that cross many of us differently and that require different resources. For example, [Queer Cartoonists](https://queercartoonists.com/) and [Black Artists](https://twitter.com/abellehayford/status/1304074678495899648?s=20) have specific directories and events that apply to them!</p>
-            <p>There’s no homogenous experience because, again, we are quite literally the rest of the world. Therefore, there are no homogeneous solutions and resources. The idea of this collective is to also connect the already existing resources in the American industry with the third world artists it’ll apply to.</p>
-            <p>In this vein, the intention of this collective is not to overtake all the space and discussion regarding third world issues. We understand that the issues in, let’s say, Brasil are not the same as in Pakistán. Therefore, we are open to connect people from the same regions who want to organize in a similar vein as us about their specific regional needs as artists.</p>
+            <h2 className="content-subtitle blue space">CONNECT, NOT DIVIDE</h2>
+            <p>The term <b className="blue">"third world"</b> is known by all it applies to. We are usually also called <b className="blue">"developing countries"</b>. We are under a specific global alignment that sees us as cheap labor that provides the first world of goods.</p>
+            <p><b className="blue">This collective is for third world artists and freelancers who are already operating in the first world industry or intend to.</b> We are usually paid in dollars or euros, even though that’s not our local currency.</p>
+            <p>That said, the third world is clearly not a monolith. <b className="blue">We are quite literally, all the world except from the USA,Canada, Western Europe, Australia and Japan.</b> And there are specific marginalizations that cross many of us differently and that require different resources. For example, <a className="blue" href="https://queercartoonists.com/">Queer Cartoonists</a> and <a className="blue" href="https://twitter.com/abellehayford/status/1304074678495899648?s=20">Black Artists</a> have specific directories and events that apply to them!</p>
+            <p>There’s no homogenous experience because, again, we are quite literally the rest of the world. Therefore, there are no homogeneous solutions and resources. <b className="blue">The idea of this collective is to also connect the already existing resources in the American industry with the third world artists it’ll apply to.</b></p>
+            <p>In this vein, <b className="blue">the intention of this collective is not to overtake all the space and discussion regarding third world issues.</b> We understand that the issues in, let’s say, Brasil are not the same as in Pakistán. Therefore, <b className="blue">we are open to connect people from the same regions who want to organize in a similar vein as us about their specific regional needs as artists.</b></p>
             <p>For example I, an artist from Argentina, won’t know much about colleges in Vietnam. But maybe several Vietnamese artists meet through this collective and want to organize for artists of their country.</p>
-            <p>A final note about industries: we know that, even though the first world dominates the market, there exist local animation, illustration and comic industries in many of our countries and many of us want to work on them as well. This collective doesn’t want to disencourage participating in local industries. The objective is to make an easier road for those who want to participate in first world industry. That said, we believe in the decentralization from USA-UK-Canada. We encourage the organization and exchange of resources for each region’s local art industries and we want to offer our space for it as well. We also are willing to connect to local collectives.</p>
+            <p>A final note about industries: we know that, even though the first world dominates the market, there exist local animation, illustration and comic industries in many of our countries and many of us want to work on them as well. <b className="blue">This collective doesn’t want to disencourage participating in local industries.</b> The objective is to make an easier road for those who want to participate in first world industry. That said, <b className="blue">we believe in the decentralization from USA-UK-Canada.</b> We encourage the organization and exchange of resources for each region’s local art industries and we want to offer our space for it as well. We also are willing to connect to local collectives.</p>
 
-            <h2 className="content-subtitle">Strength comes in working together!</h2>
+            <h2 className="content-subtitle purple space">Strength comes in working together!</h2>
 
         </>
     );
