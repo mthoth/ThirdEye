07881d4177010b415596faa5e2a5b0b1b9ae9f92
Mustafa Hijazi <hijazimt01@gmail.com>
2021-12-28 13:23:46 +0200

    optimize mobile experience

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -3,18 +3,18 @@ import React from "react";
 function Rules()
 {
     return(
-        <>
+        <div className="rulesdiv">
             <h2 className="title2">Our Rules</h2>
-            <h3>ㅤ<b className="count">1 </b>Only for artists in the Global South!</h3>
+            <b className="count">1</b><h3> Only for artists in the Global South!</h3>
             <p className="rules">(Africa, Latin America, Asia, the Middle East, Pacific Islands, etc)</p>
-            <h3><b className="count">2 </b>Be kind and be respectful</h3>
+            <b className="count">2</b><h3> Be kind and be respectful</h3>
             <p className="rules">to everyone's cultures, gender identities, religion, art styles, etc.</p>
-            <h3><b className="count">3 </b>No type of hate speech is tolerated!</h3>
+            <b className="count">3</b><h3> No type of hate speech is tolerated!</h3>
             <p className="rules">Racism, LGBTQphobia, fatphobia, xenophobia, ableism or any other type of discrimination, prejudice or mean spirited comments are not allowed and are subjective to kicking/banning</p>
-            <h3><b className="count">4 </b>No NSFW or triggering content!</h3>
+            <b className="count">4</b><h3> No NSFW or triggering content!</h3>
             <p className="rules">If really necessary, use TWs and CWs</p>
-            <h3><b className="count">5 </b>Support each other and make some art!</h3>
-        </>
+            <b className="count">5</b><h3> Support each other and make some art!</h3>
+        </div>
     );
 }
 
